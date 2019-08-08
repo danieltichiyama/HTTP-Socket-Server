d683e594412a00bbf2a6d3fe611a478d33646050
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-08-07 17:56:31 -1000

    revisions made, ready for review

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ const server = net.createServer(socket => {
   socket.on("data", chunk => {
     let arr = chunk.toString().split("\r\n");
 
+    console.log(arr);
+
     let requestObj = {};
 
-    requestObj.URI = arr[0].split(" ");
-    requestObj.host = arr[1].slice(arr[1].indexOf(":") + 2);
-    requestObj.userAgent = arr[2].slice(arr[2].indexOf(" ") + 1);
-    requestObj.accept = arr[3].slice(arr[3].indexOf(" ") + 1);
-    requestObj.message = arr[arr.length - 1];
+    arr
+      .filter(function(element) {
+        return element.length !== 0;
+      })
+      .forEach(function(element) {
+        if (element.indexOf(":") !== -1) {
+          let splitElement = element.split(":");
+          requestObj[splitElement[0]] = splitElement[1];
+        } else {
+          let splitElement = element.split(" ");
+          requestObj.URI = splitElement;
+        }
+      });
 
     let date = new Date();
 
@@ -54,12 +64,17 @@ const server = net.createServer(socket => {
 
     responseObj["Content-length"] = message.length;
 
-    let returnMsg =
-      Object.entries(responseObj).reduce(function(accumulator, current) {
-        return (accumulator += `${current[0]}: ${current[1]}\r\n`);
-      }, `${responseObj["Status Line"].join(" ")}\r\n`) + `\r\n\r\n${message}`;
+    let returnMsg = Object.entries(responseObj).reduce(function(
+      accumulator,
+      current
+    ) {
+      return (accumulator += `${current[0]}: ${current[1]}\r\n`);
+    },
+    `${responseObj["Status Line"].join(" ")}\r\n`);
 
-    socket.write(returnMsg);
+    returnMsg += `\r\n\r\n${message}`;
+
+    socket.write(returnMsg, "utf8");
     socket.end();
   });
 
@@ -68,6 +83,7 @@ const server = net.createServer(socket => {
     console.log("------------------------");
     console.log("Message: " + "", msg);
     console.log("------------------------");
+    // socket.write(msg, "utf8");
   });
 
   socket.on("error", err => {
